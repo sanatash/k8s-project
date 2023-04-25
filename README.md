b78b17367665cea39c37d17512903fea05d8644a
Anat Shulman <sanatash@gmail.com>
2023-04-25 14:35:02 +0300

    add deployment installation of rabbitmq with values from commands/rabbitmq-values.yaml file

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # k8s-project
 Full producer consumer solution on top of Kebernetes
+
+Version 1: \
+    1. ci pipeline - continious integration pipeline \
+        1.1 Clone the git repository \
+        1.2 Build docker image for producer \
+        1.3 Build docker image for consumer \
+        1.4 Push docker image for producer to docker-hub \
+        1.5 Push docker image for consumer to docker-hub
+    2. cd pipeline - continious deployment pipeline \
+        2.1 Checkout helm chart repositories for producer, consumer and rabbitmq applications \
+        2.2 Run 'helm upgrade -i' do deploy applications to the cluster
--- pipelines/cd-pipeline.groovy
+++ pipelines/cd-pipeline.groovy
@@ -18,11 +18,12 @@ spec:
 	stages {
 		stage('Run helm rabbitmq install') {
 			steps {
+			        git 'https://github.com/sanatash/k8s-project.git'
 				container('helm-rabbitmq') {
 					sh '''
 					helm repo add bitnami https://charts.bitnami.com/bitnami
 					helm repo update
-					helm upgrade -i rabbitmq bitnami/rabbitmq --set image.repository=rabbitmq,image.tag=3-management --set auth.username=admin,auth.password=secretpassword --set metrics.enabled=true
+					helm upgrade -i rabbitmq bitnami/rabbitmq -f commands/rabbitmq-values.yaml
 					helm repo add myhelmrepo https://sanatash.github.io/helm-chart/
 					helm upgrade -i producer myhelmrepo/producer
 					helm upgrade -i consumer myhelmrepo/consumer
